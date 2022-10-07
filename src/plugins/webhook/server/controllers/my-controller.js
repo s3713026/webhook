cab9ccf3f76701510d8ace779d0cd923b77766bc
AkadigitalProject <phucuong200297@gmail.com>
2022-10-07 14:42:32 +0700

    bot12

--- src/plugins/webhook/server/controllers/my-controller.js
+++ src/plugins/webhook/server/controllers/my-controller.js
@@ -13,7 +13,6 @@ module.exports = ({ strapi }) => ({
       // console.log(data.event)
       // console.log(data.sender.name)
       // console.log(data.message.text)
-      console.log(data.user.id)
       let id = '';
       let name ='';
       let message ='';
