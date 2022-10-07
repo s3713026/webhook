1475527077a849e5beca02d1ee34f54d39fcd6a6
AkadigitalProject <phucuong200297@gmail.com>
2022-10-07 16:17:03 +0700

    18

--- src/plugins/webhook/server/controllers/my-controller.js
+++ src/plugins/webhook/server/controllers/my-controller.js
@@ -10,8 +10,8 @@ module.exports = ({ strapi }) => ({
 
       let data = ctx.request.body;
       // console.log(data.sender.id)
-      console.log(data.event)
-      console.log(data.user.body)
+      // console.log(data.event)
+      console.log(data.user)
       // console.log(data.sender.name)
       // console.log(data.message.text)
       let id = '';
