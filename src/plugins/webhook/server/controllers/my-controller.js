f7948c9d27610d2ab05e3c807ba97168d49eebc2
AkadigitalProject <phucuong200297@gmail.com>
2022-10-07 17:29:41 +0700

    send

--- src/plugins/webhook/server/controllers/my-controller.js
+++ src/plugins/webhook/server/controllers/my-controller.js
@@ -136,33 +136,10 @@ module.exports = ({ strapi }) => ({
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-              "receiver": id,
-              "min_api_version": 1,
-              "sender": {
-                "name": "Aka bot "
-              },
-              "tracking_data": "tracking data",
-              "type": "text",
-              "text": `Hệ thống kiểm tra bạn chưa có vé nào`
-            })
-          };
-          request(options, function (error, response) {
-            if (error) throw new Error(error);
-            console.log(response.body);
-          });
-        } else if (message == "Hệ thống kiểm tra bạn chưa có vé nào"){
-          var options = {
-            'method': 'GET',
-            'url': 'https://chatapi.viber.com/pa/send_message',
-            'headers': {
-              'X-Viber-Auth-Token': '4feb123571e7e4b9-3fd0f7323f765805-72c5dd7ea810f8fe',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
               "receiver": "ZKBz7UV+BBmbWS0KdHjeVg==",
               "min_api_version": 7,
               "type": "text",
-              "text": "Book vé ngay và nhận ngàn ưu đãi bạn nhé",
+              "text": "Hệ thống kiểm tra bạn chưa có vé nào'\n'Book vé ngay và nhận ngàn ưu đãi bạn nhé",
               "keyboard": {
                 "Type": "keyboard",
                 "Buttons": [
